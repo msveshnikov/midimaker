a244f2051e0c1c6367b64223142c86782e7548c5
Max Sveshnikov <msveshnikov@gmail.com>
2025-06-29 07:50:23 +0200

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ import os
 
 # Configuration dictionary
 CONFIG = {
-    "gemini_model": "gemini-2.5-flash-preview-04-17", #"gemini-2.5-pro-exp-03-25", #"gemini-2.5-pro-preview-05-06" 
+    "gemini_model": "gemini-2.5-flash", #"gemini-2.5-pro"
     "api_key": os.environ.get("GEMINI_KEY"),
     "openai_model": "o4-mini", 
     "openai_api_key": os.getenv("OPENAI_KEY"), # Recommended: Load from environment
